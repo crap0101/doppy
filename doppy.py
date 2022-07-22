5d8e4151617780e1b12618a159d55fc5e78f1e2a
Marco Chieppa <crap0101@riseup.net>
2022-07-23 00:49:12 +0200

    add licence

--- doppy.py
+++ doppy.py
@@ -5,6 +5,20 @@ find duplicate files.
 # python 3.8.10
 """
 
+# Copyright (C) 2022 Marco Chieppa (aka crap0101)
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not see <http://www.gnu.org/licenses/>
+
 import argparse
 import collections
 import datetime
