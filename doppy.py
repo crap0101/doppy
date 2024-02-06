5dabdc129eba81c822ceb3fdd88c76a858b568cc
Marco Chieppa <c24p0101@gmail.com>
2024-02-06 10:53:19 +0100

    fix bug in duplicate paths, add mutiproc and threading option

--- doppy.py
+++ doppy.py
@@ -16,12 +16,15 @@
 
 import argparse
 import collections
+from collections.abc import Iterable
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 import datetime
 import fnmatch
 import functools
 import hashlib
 import itertools
 import json
+from multiprocessing import Manager
 from numbers import Number
 import operator
 import os
@@ -60,11 +63,23 @@ if vinfo.major == 3 and vinfo.minor < 10:
 else:
     realpath = functools.partial(os.path.realpath, strict=True)
 
+
+# WARNING OPTIONS
+WARN_OPT = ('ignore', 'always', 'error')
+
+################################
+# MULTIPROC AND READ CONSTANTS #
+################################
+
+DEFAULT_DEPTH = float('inf')
+READ_SIZE = 1024*64
+PROCESSOR_CHUNK = 1000
+MAX_WORKERS = None
+
+
 ###############################################
 # SOME CONSTANTS FOR PRUNE/COMPARE OPERATIONS #
 ###############################################
-
-READ_SIZE = 1024*16
 
 # see https://docs.python.org/3.8/library/os.html#os.stat_result
 STAT_PRUNE_OPTIONS = {
@@ -87,9 +102,26 @@ PRUNE_OPERATIONS_MAP = {
     '>': operator.gt,
 }
 
-WARN_OPT = ('ignore', 'always', 'error')
 
-DEFAULT_DEPTH = float('inf')
+class MPAction:
+    """For (Thread|Process)Pool"""
+    def __init__(self, fun, proxy):
+        self.fun = fun
+        self.proxy = proxy
+    
+class MPAGetHash(MPAction):
+    def __call__(self, path):
+        ret = self.fun(path)
+        if ret not in self.proxy:
+            self.proxy[ret] = [path]
+        else:
+            self.proxy[ret].append(path)
+
+class MPAFilter(MPAction):
+    def __call__(self, path):
+        if (ret := self.fun(path)):
+            self.proxy.append(ret)
+
 
 def showwarning (message, cat, fn, lno, *a, **k):
     print(message)
@@ -161,8 +193,8 @@ def _find_irregular (paths: Sequence[str]):
     raise NotImplementedError('to be written')
     #XXX+TODO: write a filter to find broken links or not stat-able files only
 
-def prune_size (paths: Sequence[str],
-                op: Callable, byte_size: int) -> Iterator[str]:
+def prune_size (op: Callable, byte_size: int,
+                paths: Sequence[str]) -> Iterator[str]:
     """
     Prune by size.
     Yields filenames from $paths for which $op(path, $byte_size) is True.
@@ -171,10 +203,10 @@ def prune_size (paths: Sequence[str],
         if op(os.stat(path).st_size, byte_size):
             yield path
         
-def prune_by_stat_attr (paths: Sequence[str],
-                        op: Callable,
+def prune_by_stat_attr (op: Callable,
                         stat_attr: str,
-                        value: Number) -> Iterator[str]:
+                        value: Number,
+                        paths: Sequence[str]) -> Iterator[str]:
     """
     Prune by stat attribute.
     Yields filenames from $paths for which
@@ -184,8 +216,8 @@ def prune_by_stat_attr (paths: Sequence[str],
         if op(getattr(os.stat(path), stat_attr), value):
             yield path
 
-def prune_pattern (paths: Sequence[str],
-                   patterns: Sequence[str]) -> Iterator[str]:
+def prune_pattern (patterns: Sequence[str],
+                   paths: Sequence[str]) -> Iterator[str]:
     """
     Prune by pattern.
     Yields paths from $paths which match any of the $patterns (use fnmatch).
@@ -196,8 +228,8 @@ def prune_pattern (paths: Sequence[str],
                 yield path
                 break
 
-def exclude_pattern (paths: Sequence[str],
-                     patterns: Sequence[str]) -> Iterator[str]:
+def exclude_pattern (patterns: Sequence[str],
+                     paths: Sequence[str]) -> Iterator[str]:
     """
     Prune by pattern.
     Yields paths from $paths which *not* match any of $patterns (use fnmatch).
@@ -206,8 +238,8 @@ def exclude_pattern (paths: Sequence[str],
         if not any(fnmatch.fnmatch(path, pattern) for pattern in patterns):
             yield path
 
-def prune_regex (paths: Sequence[str],
-                 regex: Sequence[str]) -> Iterator[str]:
+def prune_regex (regex: Sequence[str],
+                 paths: Sequence[str]) -> Iterator[str]:
     """
     Prune with regex.
     Yields paths from $paths which match any
@@ -220,8 +252,8 @@ def prune_regex (paths: Sequence[str],
                 yield path
                 break
 
-def exclude_regex (paths: Sequence[str],
-                   regex: Sequence[str]) -> Iterator[str]:
+def exclude_regex (regex: Sequence[str],
+                   paths: Sequence[str]) -> Iterator[str]:
     """
     Prune with regex.
     Yields paths from $paths which *not* match any
@@ -232,7 +264,7 @@ def exclude_regex (paths: Sequence[str],
         if not any(prog.match(path) for prog in compiled_re):
             yield path
 
-def checksum (paths: Sequence[str], hash_func_name: str) -> dict:
+def checksum (hash_func_name: Sequence[str], paths: str) -> dict:
     """
     Do checksum of path in $paths using hashlib's $hash_func_name.
     Returns a dict.
@@ -284,6 +316,34 @@ def get_parser ():
                         help='''Set warning level: choose from "%(choices)s"
                         to ignore them (no output), always print warnings or
                         raise an error. Default to "%(default)s".''')
+    # op opts:
+    mr_group = parser.add_argument_group('multiproc and read options')
+    tm_group = mr_group.add_mutually_exclusive_group()
+    tm_group.add_argument('-T', '--use-threads',
+                        dest='use_thread', action='store_true',
+                        help='''Use multithreading''')
+    tm_group.add_argument('-M', '--use-processes',
+                        dest='use_proc', action='store_true',
+                        help='''Use multiprocessing''')
+    mr_group.add_argument('-S', '--read-size',
+                          dest='read_size', type=int, default=READ_SIZE,
+                        metavar='SIZE',
+                        help='''Reads files %(metavar)s bytes at a time.
+                        If <= 0 reads files at once. Default: %(default)s''')
+    mr_group.add_argument('-c', '--chunks',
+                        dest='proc_chunk', type=int, default=PROCESSOR_CHUNK,
+                        metavar='NUM',
+                        help='''The chunks size which is submitedd to the pool
+                        as separate tasks when using the multiprocessing
+                        option. Default: %(default)s''')
+    mr_group.add_argument('-m', '--max-workers',
+                        dest='max_workers', type=int, default=MAX_WORKERS,
+                        metavar='NUM',
+                        help='''Use at most max_workers threads or processes
+                        to execute calls asynchronously. Default to %(default)s
+                        which means using default parameters
+                        (see concurrent.futures documentation).''')
+
     # filtering:
     filter_group = parser.add_argument_group('filters')
     filter_group.add_argument('-s', '--size',
@@ -383,14 +443,74 @@ def filter_time (paths, times_opts):
                 raise TypeError(
                     'invalid time: {0}: {1}'.format(e, str_val))
         filtered = prune_by_stat_attr(
-            filtered, op, STAT_PRUNE_OPTIONS[attr], val)
+            op, STAT_PRUNE_OPTIONS[attr], val, filtered)
     return filtered
 
 
-def _doit (args):
-    to_find = []
-    if args.depth <= 0:
-        args.depth = DEFAULT_DEPTH
+def exec_multi(fun, paths, executor_type,
+               chunk=PROCESSOR_CHUNK, max_workers=MAX_WORKERS):
+    with Manager() as manager:
+        lst = manager.list()
+        act = MPAFilter(fun, d)
+        with executor_type(max_workers=max_workers) as executor:
+            for _ in executor.map(act, paths, chunksize=chunk):
+                pass
+        return lst
+
+def checksum_multi(fun, paths, executor_type,
+               chunk=PROCESSOR_CHUNK, max_workers=MAX_WORKERS):
+    with Manager() as manager:
+        d = manager.dict()
+        act = MPAGetHash(fun, d)
+        with executor_type(max_workers=max_workers) as executor:
+            for _ in executor.map(act, paths, chunksize=chunk):
+                pass
+        return d
+
+def doit_multi (args):
+    to_find = set()
+    executor_type = ThreadPoolExecutor if args.threads else ProcessPoolExecutor
+    __exec_args = (executor_type, args.proc_chunk, args.max_workers)
+    for basepath in args.paths:
+        all_paths = find(expand_path(os.path.join(os.getcwd(), basepath)),
+                         args.depth)
+        regular = exec_multi(prune_regular, all_paths, *__exec_args)
+        filtered_ep = (exec_multi(functools.partial(exclude_pattern, args.exclude_patterns), regular, *__exec_args)
+                       if args.exclude_patterns else regular)
+        filtered_regex = (exec_multi(functools.partial(exclude_regex, args.exclude_regex), filtered_ep, *__exec_args)
+                          if args.exclude_regex else filtered_ep)
+        filtered_p = (exec_multi(functools.partial(prune_pattern, args.patterns), filtered_regex, *__exec_args)
+                      if args.patterns else filtered_regex)
+        filtered_regex = (exec_multi(functools.partial(prune_regex, args.regex), filtered_p, *__exec_args)
+                          if args.regex else filtered_p)
+        filtered_size = (exec_multi(functools.partial(filter_size, args.size), filtered_regex, *__exec_args)
+                         if args.size else filtered_regex)
+        filtered_time = (exec_multi(functools.partial(filter_time, args.time), filtered_size, *__exec_args)
+                         if args.time else filtered_size)
+        if args.gid is not None:
+            filtered_gid = exec_multi(
+                functools.partial(
+                    prune_by_stat_attr, operator.eq, STAT_PRUNE_OPTIONS['gid'], args.gid),
+                filtered_time,
+                *__exec_args)
+        else:
+            filtered_gid = filtered_time
+        if args.uid is not None:
+            filtered_uid = exec_multi(
+                functools.partial(
+                    prune_by_stat_attr, operator.eq, STAT_PRUNE_OPTIONS['uid'], args.uid),
+                filtered_gid,
+                *__exec_args)
+        else:
+            filtered_uid = filtered_gid
+        to_find.update(filtered_uid)
+        #get_hash (path: str, hash_type: str ='sha224', size: int =READ_SIZE)
+    return checksum_multi(
+        functools.partial(get_hash, hash_type=args.hash, size=args.read_size),
+        to_find)
+
+def doit_nomulti (args):
+    to_find = set()
     for basepath in args.paths:
         all_paths = find(expand_path(os.path.join(os.getcwd(), basepath)),
                          args.depth)
@@ -403,35 +523,43 @@ def _doit (args):
                       if args.patterns else filtered_regex)
         filtered_regex = (prune_regex(filtered_p, args.regex)
                           if args.regex else filtered_p)
-        filtered_size = filter_size(filtered_regex, args.size)
-        filtered_time = filter_time(filtered_size, args.time)
+        filtered_size = filter_size(filtered_regex, args.size) if args.size else filtered_regex
+        filtered_time = filter_time(args.time, filtered_size) if args.time else filtered_size
         if args.gid is not None:
             filtered_gid = prune_by_stat_attr(
-                filtered_time,
                 operator.eq,
                 STAT_PRUNE_OPTIONS['gid'],
-                args.gid)
+                args.gid,
+                filtered_time)
         else:
             filtered_gid = filtered_time
         if args.uid is not None:
             filtered_uid = prune_by_stat_attr(
-                filtered_gid,
                 operator.eq,
                 STAT_PRUNE_OPTIONS['uid'],
-                args.uid)
+                args.uid,
+                filtered_gid)
         else:
             filtered_uid = filtered_gid
-        to_find.append(filtered_uid)
-    return checksum(itertools.chain(*to_find), args.hash)
+        to_find.update(filtered_uid)
+    return checksum(to_find, args.hash, args.read_size)
 
 
 def main ():
     parser = get_parser()
     args = parser.parse_args()
     warnings.simplefilter(args.warn)
+
+    if args.max_workers is not None and args.max_workers < 1:
+        parser.error("max workers must be > 0")
+    if args.depth <= 0:
+        args.depth = DEFAULT_DEPTH
+    if args.read_size <= 0:
+        args.read_size = -1
+
     if args.append and args.to_json:
         warnings.warn('[BAD!] -j option ignored. To be used with -o only.')
-    results = filter_dup(_doit(args))
+    results = filter_dup(doit_nomulti(args))
     if args.output:
         outfile = open(args.output, 'w')
     elif args.append:
